4832fba0380c502b401fc3df59a34f22ad59b6ba
Angel01012 <dominguezangel1012@gmail.com>
2021-09-17 10:49:08 -0500

    Se agrega el ejercicio de fibonacci

---
+++ Proyectos java/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception 
+    {
+        int Num1=0, Num2=1,i=1,x=0,iteraciones=5;
+        System.out.println(Num1);
+        System.out.println(Num2);
+        while(iteraciones>=i)
+        {
+            x=Num1+Num2;
+            Num1=Num2;
+            Num2=x;
+            i++;
+            System.out.println(x);
+        }
+    }
+}
